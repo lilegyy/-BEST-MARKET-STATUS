3581d614abec8e3ea69e1ccb4af24e3cc131222e
lilegyy <antoine.evrard0969@gmail.com>
2024-11-17 20:50:34 +0100

    index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎮 discord.gg/bestmarket"];
+const statusMessages = ["🎧 bestmarket.sellauth.com", "🎮 discord.gg/bestmarket"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
